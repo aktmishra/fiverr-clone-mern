2fc97cf870bba82b4b7520f97eca41a472f9edbe
aktmishra <aktmishra72@gmail.com>
2023-06-05 16:14:34 +0530

    TrustedBy Component added

---
+++ src/component/trustedBy/TrustedBy.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+function TrustedBy() {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <div className="logo">
+          <span>Trusted By :</span>
+          <img src="/img/facebook(2).png" alt="" />
+          <img src="/img/google.png" alt="" />
+          <img src="/img/netflix.png" alt="" />
+          <img src="/img/p&g.png" alt="" />
+          <img src="/img/paypal.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrustedBy;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.scss";
 import Featured from "../../component/featured/Featured";
+import TrustedBy from "../../component/trustedBy/TrustedBy";
 
 const Home = () => {
   return (
     <div>
       <Featured></Featured>
-       
+       <TrustedBy></TrustedBy>
     </div>
   );
 };
